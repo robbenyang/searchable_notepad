7d05c08a241ea1e202adeae2fe260ddad1239132
robbenyang <robbenyang@gmail.com>
2013-08-12 10:06:41 +0800

    Updated search activity a little bit

--- gen/com/example/searchablenotepad/R.java
+++ gen/com/example/searchablenotepad/R.java
@@ -14,27 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Back=0x7f07000e;
-        public static final int Exit=0x7f07000b;
-        public static final int Save=0x7f07000d;
-        public static final int clear=0x7f070006;
-        public static final int create_note=0x7f07000a;
-        public static final int edit_menu=0x7f070003;
-        public static final int imgOriginal=0x7f070000;
-        public static final int main_linearlayout=0x7f070009;
-        public static final int main_listview=0x7f070007;
-        public static final int main_scrollview=0x7f070008;
-        public static final int search=0x7f07000c;
-        public static final int write_screen=0x7f070001;
-        public static final int writepad_redo=0x7f070005;
-        public static final int writepad_undo=0x7f070004;
-        public static final int writeview=0x7f070002;
+        public static final int Back=0x7f07000c;
+        public static final int Exit=0x7f070009;
+        public static final int Save=0x7f07000b;
+        public static final int clear=0x7f070005;
+        public static final int create_note=0x7f070008;
+        public static final int edit_menu=0x7f070002;
+        public static final int main_linearlayout=0x7f070007;
+        public static final int main_scrollview=0x7f070006;
+        public static final int search=0x7f07000a;
+        public static final int write_screen=0x7f070000;
+        public static final int writepad_redo=0x7f070004;
+        public static final int writepad_undo=0x7f070003;
+        public static final int writeview=0x7f070001;
     }
     public static final class layout {
-        public static final int gridview=0x7f030000;
-        public static final int handwriting=0x7f030001;
-        public static final int main_activity=0x7f030002;
-        public static final int main_scrollview=0x7f030003;
+        public static final int handwriting=0x7f030000;
+        public static final int main_scrollview=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f060000;
